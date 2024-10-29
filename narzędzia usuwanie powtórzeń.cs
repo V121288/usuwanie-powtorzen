b2ccc19541273a7f779f326ea79e1848d5549b15
V121288 <kacperwilda@proton.me>
2024-10-29 20:09:48 +0100

    Update narzędzia usuwanie powtórzeń.cs

--- narzędzia usuwanie powtórzeń.cs
+++ narzędzia usuwanie powtórzeń.cs
@@ -10,6 +10,7 @@ internal class Program
         Console.WriteLine("Wpisz zdanie");
         string zdanie = Console.ReadLine();
         string usunietePowtorzenia = usuwaniePowtorzen(zdanie);
+        Console.WriteLine("Skrócone zdanie:");
         Console.WriteLine(usunietePowtorzenia);
         int iloscSamoglosek = liczenieSamogłosek(usunietePowtorzenia);
         Console.WriteLine("Ilość samogłosek:");
@@ -19,19 +20,19 @@ internal class Program
     public static string usuwaniePowtorzen(string zdanie)
     {
         string skroconeZdanie = "";
-        if (string.IsNullOrEmpty(zdanie)) 
+        if (string.IsNullOrEmpty(zdanie))
         {
             return "";
         }
         bool[] boole = new bool[zdanie.Length];
         for (int i = 0; i < zdanie.Length - 1; i++)
         {
-         
+
             if (zdanie[i] == zdanie[i + 1])
             {
                 boole[i] = true;
             }
-            
+
         }
         for (int i = 0; i < zdanie.Length; i++)
         {
@@ -39,11 +40,11 @@ internal class Program
             {
                 skroconeZdanie += Convert.ToString(zdanie[i]);
             }
-            
+
         }
         return skroconeZdanie;
-        
-       
+
+
 
 
 
@@ -53,18 +54,18 @@ internal class Program
 
     public static int liczenieSamogłosek(string literki)
     {
-        if (string.IsNullOrEmpty(literki)) 
+        if (string.IsNullOrEmpty(literki))
         {
             return 0;
         }
         int licznik = 0;
         char[] samogloski = { 'a', 'ą', 'e', 'ę', 'i', 'o', 'u', 'ó', 'y', 'A', 'Ą', 'E', 'Ę', 'I', 'O', 'U', 'Ó', 'Y' };
-        for (int i = 0; i < literki.Length; i++) 
+        for (int i = 0; i < literki.Length; i++)
         {
- 
-            for (int j = 0; j < samogloski.Length; j++) 
+
+            for (int j = 0; j < samogloski.Length; j++)
             {
-                if (literki[i] == samogloski[j]) 
+                if (literki[i] == samogloski[j])
                 {
                     licznik++;
                 }
